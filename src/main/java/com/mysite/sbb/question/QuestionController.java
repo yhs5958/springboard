6dc580c2e9ba45d86ad83bfa3f1debe68b74e828
yhs5958 <yhs5958@email.com>
2024-05-07 18:45:59 +0900

    페이징 구현 수정 및 사소한 작업.

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -31,7 +31,7 @@ public class QuestionController {
 	private final UserService userService;
 
 	@GetMapping("/list")
-	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+	public String list(Model model, @RequestParam(value = "page", defaultValue = "-1") int page,
 			@RequestParam(value = "kw", defaultValue = "") String kw) {
 		Page<Question> paging = this.questionService.getList(page, kw);
 		model.addAttribute("paging", paging);

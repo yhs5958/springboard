6733c12afa86e0b158a710f98d644f1249668e34
yhs5958 <yhs5958@email.com>
2024-05-10 15:29:58 +0900

    페이징 오류 수정

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,7 +3,6 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
@@ -11,12 +10,13 @@ class SbbApplicationTests {
 
     @Autowired
     private QuestionService questionService;
-
+    
     @Test
     void testJpa() {
-        for (int i = 1; i <= 20; i++) {
-            String subject = String.format("테스트 글 %02d번", i);
-            String content = String.format("%02d번째 글입니다.", i);;
+        for (int i = 1; i <= 60; i++) {
+        	
+            String subject = String.format("테스트 [%02d]번 ", i);
+            String content = String.format("%02d번째 글.", i);;
             this.questionService.create(subject, content, null);
         }
     }
